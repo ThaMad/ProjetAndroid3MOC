5a4e95c7fd55ee719953607f9820e7349f76fb2c
ThaMad <thanudi.m@gmail.com>
2023-07-19 20:50:48 +0200

    Initial commit

---
+++ app/src/main/java/com/example/theaudiodbproject/ui/ranking/RankingFragment.kt
@@ -0,0 +1,42 @@
+package com.example.theaudiodbproject.ui.ranking
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.theaudiodbproject.databinding.FragmentRankingBinding
+
+class RankingFragment : Fragment() {
+
+    private var _binding: FragmentRankingBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val rankingViewModel =
+            ViewModelProvider(this).get(RankingViewModel::class.java)
+
+        _binding = FragmentRankingBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textHome
+        rankingViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
